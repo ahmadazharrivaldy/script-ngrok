02cd53edc691e4250c7115f80cc73212f6f28bb1
ahmadazharrivaldy <87871737+ahmadazharrivaldy@users.noreply.github.com>
2025-02-13 07:40:31 +0700

    Update and rename ngrok.txt to ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,8 @@
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt update \
+	&& sudo apt install ngrok
+
+ngrok config add-authtoken 2KCGoWYIA8FLKGpeVlXnj2e0fT9_6wWtEHMxiLCo8eCUS8Y9o
