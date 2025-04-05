6118c248c0aef92fde043c9d2f9728c9d817378d
ahmadazharrivaldy <87871737+ahmadazharrivaldy@users.noreply.github.com>
2025-04-05 17:20:07 +0700

    Update install-ngrok.sh

--- install-ngrok.sh
+++ install-ngrok.sh
@@ -8,3 +8,7 @@ sudo tar -xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
 
 echo "Configuring ngrok with authtoken..."
 ngrok config add-authtoken 2KCGoWYIA8FLKGpeVlXnj2e0fT9_6wWtEHMxiLCo8eCUS8Y9o
+
+echo "Starting ngrok..."
+cp ngrok.service /etc/systemd/system/ngrok.service
+systemctl enable --now ngrok.service
